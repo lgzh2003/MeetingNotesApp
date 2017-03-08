c34db5e2e9818c1d478b53bc4b3ff4ca0695e93f
lgzh2003 <maxishandson@gmail.com>
2017-03-08 00:14:59 -0700

    add validation in models

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var notesCtrl = require('../controllers/notes.Ctrl');
+var asyncCtrl = require('../controllers/async.Ctrl');
 
-router.get('/',function(req,res){
-	res.render('index');
-});
+router.get('/', asyncCtrl.homePage);
+router.post('/', notesCtrl.noteByMember);
 
-router.get('/newnote',function(req,res){
-	res.render('newnote');
-});
+router.get('/newnote', notesCtrl.allUsersNotes);
+router.post('/newnote', notesCtrl.createNote);
 
 module.exports = router;
